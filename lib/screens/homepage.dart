a4bd8384f2f2766b4dc8a81cad1fa997a5d45f9d
mahima <076bct033.mahima@pcampus.edu.np>
2021-11-25 22:54:58 +0545

    logo changed

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_app/screens/wigets.dart';
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
 
@@ -13,12 +13,18 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
        child:Container(
-         padding: const EdgeInsets.all(20),
+
+         padding: const EdgeInsets.symmetric(
+           horizontal: 24.0,
+           vertical: 32.0,
+         ),
           
         child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,  //sab left bata suru hunxa
           children: const [ 
             Image(image: AssetImage(
               'assets/images/logo.png'),),
+              Taskaddwidget(),
           ]
 
         )
---
+++ lib/screens/wigets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Taskaddwidget extends StatelessWidget {
+  const Taskaddwidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Taskaddwidget'),
+      
+    );
+  }
+}
